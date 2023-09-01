1cda7b6c9579af36c6864d2f30a70d0d363a3e7a
Tirth Govani <85156527+tirth3980@users.noreply.github.com>
2023-09-01 12:21:33 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Calcuator from "./components/calculator";
+
+function App() {
+  return (
+    <div className="container">
+      <Calcuator />
+    </div>
+  );
+}
+
+export default App;
